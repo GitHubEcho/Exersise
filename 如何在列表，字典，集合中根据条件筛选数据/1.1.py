ec77e41d80cc260ea9ad8c520e17e67aca66f898
GitHubEcho <1187984211@qq.com>
2017-06-26 10:50:16 +0800

    Initial commit

---
+++ 如何在列表，字典，集合中根据条件筛选数据/1.1.py
@@ -0,0 +1,10 @@
+#!/bin/python
+#coding:utf-8
+""""""
+data = [1,5,-3,-2,6,0,9]
+res = []
+for n in data:
+    if n < 0:
+        res.append(n)
+
+print(res)
---
+++ 如何在列表，字典，集合中根据条件筛选数据/1.2.py
@@ -0,0 +1,6 @@
+#!/bin/python
+#coding:utf-8
+"""列表中筛选数据"""
+from random import randint
+data = [randint(-10,10)for _ in xrange(10)] 
+print(data)
