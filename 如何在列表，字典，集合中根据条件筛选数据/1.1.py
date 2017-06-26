93dd8fe9575bc539f66b76c165f0ef698d26af81
GitHubEcho <1187984211@qq.com>
2017-06-26 13:04:09 +0800

    06.26 13.03

--- 如何在列表，字典，集合中根据条件筛选数据/1.1.py
+++ 如何在列表，字典，集合中根据条件筛选数据/1.1.py
@@ -1,6 +1,6 @@
 #!/bin/python
 #coding:utf-8
-""""""
+"""迭代方式输出"""
 data = [1,5,-3,-2,6,0,9]
 res = []
 for n in data:
--- 如何在列表，字典，集合中根据条件筛选数据/1.2.py
+++ 如何在列表，字典，集合中根据条件筛选数据/1.2.py
@@ -2,5 +2,19 @@
 #coding:utf-8
 """列表中筛选数据"""
 from random import randint
-data = [randint(-10,10)for _ in xrange(10)] 
+data = [randint(-10,10)for _ in xrange(10)]
 print(data)
+
+#fliter函数筛选
+print filter(lambda x:x >= 0,data)
+
+
+#列表过滤
+print [x for x in data if x >= 0]
+
+
+"""note:
+# 1.lambda 会创建一个函数对象，，但是不会把函数的对对象赋值给标识符，儿def则会把函数对象赋值给一个变量。
+# 2.lambda 是一个表达式，而def是一个语句。
+"""
+
