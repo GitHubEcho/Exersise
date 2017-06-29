6377ec3aa723a7d83cfa53025fdd89a50572f89d
GitHubEcho <1187984211@qq.com>
2017-06-29 23:52:21 +0800

    kk

--- 老男孩python/day 1/sign_in.py
+++ 老男孩python/day 1/sign_in.py
@@ -5,7 +5,26 @@
 登录接口
 思路：
  1.用户登录时查询登录文件是否密码输入正确
- 2.
-
+ 2.用户输入密码错误三次时，加入黑名单（文件夹）
 
 """
+
+def sign_in(user,passwd):
+    if user in s and passwd in s:
+
+def verify():
+
+
+
+
+
+
+
+
+
+
+
+if __name__ == '__main__':
+    username = input("your name :")
+    passwd = input("your passwd:")
+    sign_in(username,passwd)
