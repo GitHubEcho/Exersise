7bab73f60ae27b71e3894e55a547da3292d7d303
GitHubEcho <1187984211@qq.com>
2017-07-13 13:40:15 +0800

    china_map

--- Oldboy_python/day5/calculator.py
+++ Oldboy_python/day5/calculator.py
@@ -1,7 +1,8 @@
 #!/usr/bin/env python3
 #coding:utf-8
+import re
 
 s = '1 - 2 * ( (60-30 +(-40/5) * (9-2*5/3 + 7 /3*99/4*2998 +10 * 568/14 )) - (-4*3)/ (16-3*2) )'
 expression = input('please input your number:')
-list_expression = list(expression)
-re.search(r'\([^()]+\)',l).group()
+list_expression = [x for x in s ]
+re.search(r'\([^()]+\)',s).group()
